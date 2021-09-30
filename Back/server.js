6fed181f788ab0162b71b30b21fb343adf3c8356
dnjzm108 <dnjzm108@kakao.com>
2021-09-30 15:26:49 +0900

    sql

--- Back/server.js
+++ Back/server.js
@@ -2,7 +2,35 @@ const express = require('express');
 const app = express();
 require('dotenv');
 const PORT = process.env.PORT || 3000;
+const mysql = require('mysql')
 
-app.listen(port,()=>{
+
+let connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'0000',
+    database:'grootcoin'
+})
+connection.connect();
+
+const pool = mysql.createPool ({
+    host:'localhost',
+    user:'root',
+    password:'0000',
+    database:'grootcoin',
+    connectionLimit:100
+})
+
+app.get('/',(req,res)=>{
+    connection.query('create table success ( ID int not null)',(error,result)=>{
+        if(error){
+            console.log(error);
+        }else{
+            console.log(result);
+        }
+    })
+})
+
+app.listen(PORT,()=>{
     console.log(`server port ${PORT}`)
 })
