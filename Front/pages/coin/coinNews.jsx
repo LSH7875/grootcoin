10164e0670f917c2a35bcb17f9624aa09d636315
jihyun <kjwise1128@gmail.com>
2021-10-04 19:11:51 +0900

    코인뉴스 크롤링 완성~~~~!!!!!

--- Front/pages/coin/coinNews.jsx
+++ Front/pages/coin/coinNews.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/header"
 import CoinNews from "../../components/coinNews"
+import Footer from "../../components/footer"
 
 const coinNews = () =>{
     return(
         <>
             <Header />
             <CoinNews />
+            <Footer />
         </>
     )
 }
