5f9625da673990f772eabde878f2d3c347470c65
jorny95 <kjwise1128@gmail.comgit config --global user.name jorny95git config --global user.email kjwise1128@gmail.com>
2021-10-08 17:49:50 +0900

    하나만 업데이트 되게끔 ws 수정

--- Back/routers/coin/coin.controller.js
+++ Back/routers/coin/coin.controller.js
@@ -251,7 +251,7 @@ let graph = async (req, res) => {
         data.push({
             half_max: halfhour_price[0][0].max,
             half_min: halfhour_price[0][0].min,
-            half_start: halfhour_data[0][0].payment,
+            // half_start: halfhour_data[0][0].payment,
             half_last: halfhour_data[0][halfhour_data.length - 1].payment,
             time:search_holfhour
         })
