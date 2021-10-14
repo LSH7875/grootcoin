3055a6a391d8c1f8849787b802696ad80e937163
jorny95 <kjwise1128@gmail.comgit config --global user.name jorny95git config --global user.email kjwise1128@gmail.com>
2021-10-14 13:49:57 +0900

    자산 부분 css 수정, 자산 ws 되게 해놨습니다.

--- Back/routers/user/index.js
+++ Back/routers/user/index.js
@@ -5,5 +5,6 @@ const controller = require('./user.controller');
 router.post('/join_success', controller.join_success);
 router.post('/login_success', controller.login_success);
 router.get('/login_check',controller.login_check)
+router.get('/assets',controller.assets)
 
 module.exports = router;
