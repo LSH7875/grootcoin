7e2df9a30641d1cd19a7d6b3e72054550c5cfe14
dnjzm108 <dnjzm108@kakao.com>
2021-10-19 17:01:38 +0900

    add_garadata

--- Back/graph_data.js
+++ Back/graph_data.js
@@ -1,297 +1,298 @@
 let graph_data = [
-     {      time:1538778600000,
-            half_start:6629.81,
-            half_max:6650.5,
-            half_min:6623.04,
-            half_last:6633.33
-        },
-          {
-            time:1538780400000,
-            half_start:6632.01,
-            half_max:6643.5,
-            half_min:6620,
-            half_last:6630.11
-          },
-          {
-            time:1538782200000,
-            half_start:6630.71,
-            half_max:6648.9,
-            half_min:6623.34,
-            half_last:6635.65
-          },
-          {
-            time:1538784000000,
-            half_start:6635.65,
-            half_max:6651,
-            half_min:6629.67,
-            half_last:6638.24
-          },
-          {
-            time:1538785800000,
-            half_start:6638.24,
-            half_max:6640,
-            half_min:6620,
-            half_last:6624.47
-          },
-          {
-            time:1538787600000,
-            half_start:6624.53,
-            half_max:6636.0,
-            half_min:6621.68,
-            half_last:6624.31
-          },
-          {
-            time:1538789400000,
-            half_start:6624.61,
-            half_max:6632.2,
-            half_min:6617,
-            half_last:6626.02
-          },
-          {
-            time:1538791200000,
-            half_start:6627,
-            half_max:7620,
-            half_min:6584.22,
-            half_last:6603.02
-          },
-          {
-            time:1538793000000,
-            half_start:6605,
-            half_max:6689.03,
-            half_min:6598.95,
-            half_last:6604.01
-          },
-          {
-            time:1538794800000,
-            half_start:6604.5,
-            half_max:6914.4,
-            half_min:6602.26,
-             half_last:6608.02
-          },
-          {
-            time:1538796600000,
-            half_start:6608.02,
-            half_max:6610.6,
-            half_min:6601.99,
-            half_last:6608.91
-          },
-          {
-            time:1538798400000,
-            half_start:6608.91,
-            half_max:6618.9,
-            half_min:6608.0,
-            half_last:6612
-          },
-          {
-            time:1538800200000,
-            half_start:6612,
-            half_max:6965.13,
-            half_min:6605.0,
-            half_last:6612
-          },
-          {
-            time:1538802000000,
-            half_start:6612,
-            half_max:6964.12,
-            half_min:6608.43,
-            half_last:6622.95
-          },
-          {
-            time:1538803800000,
-            half_start:6623.91,
-            half_max:6623.9,
-            half_min:6615,
-            half_last:6615.67
-          },
-          {
-            time:1538805600000,
-            half_start:6618.69,
-            half_max:6618.7,
-            half_min:6610,
-            half_last:6610.4
-          },
-          {
-            time:1538807400000,
-            half_start:6611,
-            half_max:6992.78,
-            half_min:6610.4,
-            half_last:6614.9
-          },
-          {
-            time:1538809200000,
-            half_start:6614.9,
-            half_max:6926.2,
-            half_min:6613.33,
-            half_last:6623.45
-          },
-          {
-            time:1538811000000,
-            half_start:6623.48,
-            half_max:6627,
-            half_min:6618.38,
-            half_last:6620.35
-          },
-          {
-            time:1538812800000,
-            half_start:6619.43,
-            half_max:6620.3,
-            half_min:6610.05,
-            half_last:6615.53
-          },
-          {
-            time:1538814600000,
-            half_start:6615.53,
-            half_max:6617.9,
-            half_min:6610,
-            half_last:6615.19
-          },
-          {
-            time:1538816400000,
-            half_start:6615.19,
-            half_max:6621.6,
-            half_min:6608.2,
-            half_last:6620
-          },
-          {
-            time:1538818200000,
-            half_start:6619.54,
-            half_max:6625.1,
-            half_min:6614.1,
-            half_last:6620
-          },
-          {
-            time:1538820000000,
-            half_start:6620.33,
-            half_max:6634.1,
-            half_min:6617.24,
-            half_last:6624.61
-          },
-          {
-            time:1538821800000,
-            half_start:6625.95,
-            half_max:6626,
-            half_min:6611.66,
-            half_last:6617.58
-          },
-          {
-            time:1538823600000,
-            half_start:6619,
-            half_max:6795.97,
-            half_min:6595.27,
-            half_last:6598.86
-          },
-          {
-            time:1538825400000,
-            half_start:6598.86,
-            half_max:6598.8,
-            half_min:6570,
-            half_last:6587.16
-          },
-          {
-            time:1538827200000,
-            half_start:6588.86,
-            half_max:6600,
-            half_min:6580,
-            half_last:6593.4
-          },
-          {
-            time:1538829000000,
-            half_start:6593.99,
-            half_max:6598.8,
-            half_min:6585,
-            half_last:6587.81
-          },
-          {
-            time:1538830800000,
-            half_start:6587.81,
-            half_max:6592.7,
-            half_min:6567.1,
-            half_last:6578
-          },
-          {
-            time:1538832600000,
-            half_start:6578.35,
-            half_max:6581.7,
-            half_min:6567.3,
-            half_last:6579
-          },
-          {
-            time:1538834400000,
-            half_start:6579.38,
-            half_max:6580.9,
-            half_min:6566.77,
-            half_last:6575.96
-          },
-          {
-            time:1538836200000,
-            half_start:6575.96,
-            half_max:6589,
-            half_min:6571.77,
-            half_last:6588.92
-          },
-          {
-            time:1538838000000,
-            half_start:6588.92,
-            half_max:6594,
-            half_min:6577.55,
-            half_last:6589.22
-          },
-          {
-            time:1538839800000,
-            half_start:6589.3,
-            half_max:6698.89,
-            half_min:6589.1,
-            half_last:6596.08
-          },
-          {
-            time:1538841600000,
-            half_start:6597.5,
-            half_max:6006,
-            half_min:5880.39,
-            half_last:6596.25
-          },
-          {
-            time:1538843400000,
-            half_start:6598.03,
-            half_max:6600,
-            half_min:6588.73,
-            half_last:6595.97
-          },
-          {
-            time:1538845200000,
-            half_start:6595.97,
-            half_max:6602.0,
-            half_min:6588.1,
-            half_last:6602
-          },
-          {
-            time:1538847000000,
-            half_start:6602,
-            half_max:6607,
-            half_min:6358,
-            half_last:6599.95
-          },
-          {
-            time:1538848800000,
-            half_start:6600.63,
-            half_max:6601.2,
-            half_min:6590.39,
-            half_last:6591.02
-          },
-          {
-            time:1538850600000,
-            half_start:6591.02,
-            half_max:6603.0,
-            half_min:6591,
-            half_last:6591
-          },
-          {
-            time:1538852400000,
-            half_start:6591,
-            half_max:6603.32,
-            half_min:6585,
-            half_last:6592
-          }
+  {
+    time: 1538778600000,
+    half_start: 3029.81,
+    half_max: 6650.5,
+    half_min: 6623.04,
+    half_last: 4533.33
+  },
+  {
+    time: 1538780400000,
+    half_start: 4533.01,
+    half_max: 5043.5,
+    half_min: 4020,
+    half_last: 5030.11
+  },
+  {
+    time: 1538782200000,
+    half_start: 5030.71,
+    half_max: 5048.9,
+    half_min: 2523.34,
+    half_last: 5035.65
+  },
+  {
+    time: 1538784000000,
+    half_start: 5035.65,
+    half_max: 6051,
+    half_min: 4629.67,
+    half_last: 4638.24
+  },
+  {
+    time: 1538785800000,
+    half_start: 4638.24,
+    half_max: 6640,
+    half_min: 3020,
+    half_last: 6624.47
+  },
+  {
+    time: 1538787600000,
+    half_start: 6624.53,
+    half_max: 7636.0,
+    half_min: 6621.68,
+    half_last: 6624.31
+  },
+  {
+    time: 1538778600000,
+    half_start: 6624.81,
+    half_max: 6650.5,
+    half_min: 3023.04,
+    half_last: 4533.33
+  },
+  {
+    time: 1538780400000,
+    half_start: 4533.01,
+    half_max: 5043.5,
+    half_min: 4020,
+    half_last: 5030.11
+  },
+  {
+    time: 1538782200000,
+    half_start: 5030.71,
+    half_max: 5048.9,
+    half_min: 2523.34,
+    half_last: 5035.65
+  },
+  {
+    time: 1538784000000,
+    half_start: 5035.65,
+    half_max: 6051,
+    half_min: 4629.67,
+    half_last: 4638.24
+  },
+  {
+    time: 1538785800000,
+    half_start: 4638.24,
+    half_max: 8640,
+    half_min: 2620,
+    half_last: 5000.47
+  },
+  {
+    time: 1538778600000,
+    half_start: 3029.81,
+    half_max: 6650.5,
+    half_min: 6623.04,
+    half_last: 4533.33
+  },
+  {
+    time: 1538780400000,
+    half_start: 4533.01,
+    half_max: 5043.5,
+    half_min: 4020,
+    half_last: 5030.11
+  },
+  {
+    time: 1538782200000,
+    half_start: 5030.71,
+    half_max: 5048.9,
+    half_min: 2523.34,
+    half_last: 5035.65
+  },
+  {
+    time: 1538784000000,
+    half_start: 5035.65,
+    half_max: 6051,
+    half_min: 4629.67,
+    half_last: 4638.24
+  },
+  {
+    time: 1538785800000,
+    half_start: 4638.24,
+    half_max: 6640,
+    half_min: 3020,
+    half_last: 6624.47
+  },
+  {
+    time: 1538787600000,
+    half_start: 6624.53,
+    half_max: 7636.0,
+    half_min: 6621.68,
+    half_last: 6624.31
+  },
+  {
+    time: 1538778600000,
+    half_start: 6624.81,
+    half_max: 6650.5,
+    half_min: 3023.04,
+    half_last: 4533.33
+  },
+  {
+    time: 1538780400000,
+    half_start: 4533.01,
+    half_max: 5043.5,
+    half_min: 4020,
+    half_last: 5030.11
+  },
+  {
+    time: 1538782200000,
+    half_start: 5030.71,
+    half_max: 5048.9,
+    half_min: 2523.34,
+    half_last: 5035.65
+  },
+  {
+    time: 1538784000000,
+    half_start: 5035.65,
+    half_max: 6051,
+    half_min: 4629.67,
+    half_last: 4638.24
+  },
+  {
+    time: 1538785800000,
+    half_start: 4638.24,
+    half_max: 5640,
+    half_min: 2620,
+    half_last: 5000.47
+  },
+  {
+    time: 1538818200000,
+    half_start: 4500.54,
+    half_max: 6625.1,
+    half_min: 2000.1,
+    half_last: 2100
+  },
+  {
+    time: 1538820000000,
+    half_start: 2100.33,
+    half_max: 3000.1,
+    half_min: 2000.24,
+    half_last: 2500.61
+  },
+  {
+    time: 1538821800000,
+    half_start: 2500.95,
+    half_max: 3500,
+    half_min: 2000.66,
+    half_last: 2200.58
+  },
+  {
+    time: 1538823600000,
+    half_start: 1500,
+    half_max: 2000.97,
+    half_min: 1330.27,
+    half_last: 1600.86
+  },
+  {
+    time: 1538825400000,
+    half_start: 1600.86,
+    half_max: 2000.8,
+    half_min: 1300,
+    half_last: 1310.16
+  },
+  {
+    time: 1538827200000,
+    half_start: 1200.86,
+    half_max: 1800,
+    half_min: 900,
+    half_last: 950.4
+  },
+  {
+    time: 1538829000000,
+    half_start: 800.99,
+    half_max: 900.8,
+    half_min: 700,
+    half_last: 850.81
+  },
+  {
+    time: 1538830800000,
+    half_start: 850.81,
+    half_max: 1300.7,
+    half_min: 840.1,
+    half_last: 1200
+  },
+  {
+    time: 1538832600000,
+    half_start: 1300.35,
+    half_max: 1500.7,
+    half_min: 1200.3,
+    half_last: 1400
+  },
+  {
+    time: 1538834400000,
+    half_start: 900.38,
+    half_max: 910.9,
+    half_min: 500.77,
+    half_last: 650.96
+  },
+  {
+    time: 1538836200000,
+    half_start: 650.96,
+    half_max: 700,
+    half_min: 400.77,
+    half_last: 450.92
+  },
+  {
+    time: 1538838000000,
+    half_start: 450.92,
+    half_max: 600,
+    half_min: 300.55,
+    half_last: 310.22
+  },
+  {
+    time: 1538839800000,
+    half_start: 200.3,
+    half_max: 300.89,
+    half_min: 105.1,
+    half_last: 150.08
+  },
+  {
+    time: 1538841600000,
+    half_start: 150.5,
+    half_max: 300,
+    half_min: 100.39,
+    half_last: 150.25
+  },
+  {
+    time: 1538843400000,
+    half_start: 150.03,
+    half_max: 152,
+    half_min: 130.73,
+    half_last: 135.97
+  },
+  {
+    time: 1538845200000,
+    half_start: 50.97,
+    half_max: 70.0,
+    half_min: 20.1,
+    half_last: 30
+  },
+  {
+    time: 1538847000000,
+    half_start: 30,
+    half_max: 50,
+    half_min: 20,
+    half_last: 29.95
+  },
+  {
+    time: 1538848800000,
+    half_start: 29.63,
+    half_max: 40.2,
+    half_min: 10.39,
+    half_last: 15.02
+  },
+  {
+    time: 1538850600000,
+    half_start: 15.02,
+    half_max: 800.0,
+    half_min: 10,
+    half_last: 700
+  },
+  {
+    time: 1538852400000,
+    half_start: 700,
+    half_max: 6603.32,
+    half_min: 6585,
+    half_last: 6592
+  }
 ]
 
-module.exports ={graph_data}
+module.exports = { graph_data }
