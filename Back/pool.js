9abacd7191bce50d96d43628a07c1b76a6bf1599
LSH7875 <80299798+LSH7875@users.noreply.github.com>
2021-11-27 22:09:09 +0900

    Fix pool.js
    update pool_password

--- Back/pool.js
+++ Back/pool.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '0000',
+    password: 'grootcoinsuccess',
     database: 'grootcoin',
     connectionLimit: 100
 })
